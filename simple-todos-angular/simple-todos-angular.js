ffe88390eac05023f49a39a6898c4af4534a40a4
Adrien Bettini <ninigeek@gmail.com>
2015-07-28 14:03:51 +0200

    [NEW] Security with methods

--- simple-todos-angular/simple-todos-angular.js
+++ simple-todos-angular/simple-todos-angular.js
@@ -6,23 +6,35 @@ if (Meteor.isClient) {
         passwordSignupFields: "USERNAME_ONLY"
     });
 
-    //This code only runs on the client
+    // This code only runs on the client
     angular.module('simple-todos',['angular-meteor']);
 
-    angular.module('simple-todos').controller('TodosListCtrl',['$scope', '$meteor',
+    function onReady() {
+        angular.bootstrap(document, ['simple-todos']);
+    }
+
+    if (Meteor.isCordova)
+        angular.element(document).on('deviceready', onReady);
+    else
+        angular.element(document).ready(onReady);
+
+    angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
         function ($scope, $meteor) {
 
-            $scope.tasks = $meteor.collection( function () {
-                return Tasks.find($scope.getReactively('query'), { sort: { createdAt: -1 } })
+            $scope.tasks = $meteor.collection(function() {
+                return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
             });
 
             $scope.addTask = function (newTask) {
-                $scope.tasks.push({
-                    text: newTask,
-                    createdAt: new Date(),
-                    owner: Meteor.userId(),
-                    username: Meteor.user().username
-                });
+                $meteor.call('addTask', newTask);
+            };
+
+            $scope.deleteTask = function (task) {
+                $meteor.call('deleteTask', task._id);
+            };
+
+            $scope.setChecked = function (task) {
+                $meteor.call('setChecked', task._id, !task.checked);
             };
 
             $scope.$watch('hideCompleted', function() {
@@ -35,6 +47,28 @@ if (Meteor.isClient) {
             $scope.incompleteCount = function () {
                 return Tasks.find({ checked: {$ne: true} }).count();
             };
-        }
-    ]);
+
+        }]);
 }
+
+Meteor.methods({
+    addTask: function (text) {
+        // Make sure the user is logged in before inserting a task
+        if (! Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.insert({
+            text: text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    },
+    deleteTask: function (taskId) {
+        Tasks.remove(taskId);
+    },
+    setChecked: function (taskId, setChecked) {
+        Tasks.update(taskId, { $set: { checked: setChecked} });
+    }
+});
