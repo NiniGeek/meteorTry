958b2982c5c4011dcbd0c0b02392662c8d33cb8d
Adrien Bettini <ninigeek@gmail.com>
2015-07-29 13:42:34 +0200

    [NEW] Use of ng-annotate. angular-meteor uses that process automatically. Change your .js files to end with .ng.js.

--- socially/app.ng.js
+++ socially/app.ng.js
@@ -1,9 +1,8 @@
 if (Meteor.isClient) {
     angular.module('socially',['angular-meteor']);
 
-    angular.module('socially').controller('PartiesListCtrl', ['$scope',
+    angular.module('socially').controller('PartiesListCtrl',
         function($scope){
-
             $scope.parties = [
                 {'name': 'Dubstep-Free Zone',
                  'description': 'Can we please just for an evening not listen to dubstep.'},
@@ -12,6 +11,6 @@ if (Meteor.isClient) {
                 {'name': 'Savage lounging',
                  'description': 'Leisure suit required. And only fiercest manners.'}
             ];
-
-        }]);
+        }
+    );
 }
