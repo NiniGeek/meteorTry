2f7bc313d241fc8fd19a0d6e8c9c22eab2b5f571
Adrien Bettini <ninigeek@gmail.com>
2015-07-30 13:50:13 +0200

    [NEW] Privacy and publish-subscribe functions

--- socially/model/parties.js
+++ socially/model/parties.js
@@ -11,3 +11,17 @@ Parties.allow({
         return userId && party.owner === userId;
     }
 });
+
+Meteor.publish("parties", function () {
+    return Parties.find({
+        $or:[
+            {$and:[
+                {"public": true},
+                {"public": {$exists: true}}
+            ]},
+            {$and:[
+                {owner: this.userId},
+                {owner: {$exists: true}}
+            ]}
+        ]});
+});
